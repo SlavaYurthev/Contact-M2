6f08e0921540dfdc13cebef50deb5143281e1e86
Alexander Menk <a.menk@imi.de>
2022-12-13 08:05:41 +0100

    Only weakly depend on ReCaptcha
    * We might not want to use ReCaptcha, so we do not hard-depend on it
      to make this compatible with systems were recaptcha is removed
      by composer

--- Observer/RecaptchaFormObserver.php
+++ Observer/RecaptchaFormObserver.php
@@ -2,47 +2,53 @@
 
 namespace SY\Contact\Observer;
 
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\App\Response\Http;
 use Magento\Framework\App\Response\RedirectInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\ReCaptchaUi\Model\IsCaptchaEnabledInterface;
-use Magento\ReCaptchaUi\Model\RequestHandlerInterface;
 
 class RecaptchaFormObserver implements ObserverInterface
 {
     private const RECAPTCHA_FORM_KEY = 'sy_contact_form';
 
-    private IsCaptchaEnabledInterface $isCaptchaEnabled;
+    private $isCaptchaEnabled = null;
 
-    private RequestHandlerInterface $requestHandler;
+    private $requestHandler = null;
 
     private RedirectInterface $redirect;
 
     private Http $httpResponse;
 
     public function __construct(
-        IsCaptchaEnabledInterface $isCaptchaEnabled,
-        RequestHandlerInterface $requestHandler,
+        ObjectManagerInterface $objectManager,
         RedirectInterface $redirect,
         Http $httpResponse
     ) {
-        $this->isCaptchaEnabled = $isCaptchaEnabled;
-        $this->requestHandler = $requestHandler;
+        try {
+            $this->isCaptchaEnabled = $objectManager->get('Magento\ReCaptchaUi\Model\IsCaptchaEnabledInterface');
+            $this->requestHandler = $objectManager->get('Magento\ReCaptchaUi\Model\RequestHandlerInterface');
+        } catch (\ReflectionException $e) {
+            // ReCaptcha not available - we only weakly depend on it
+        }
         $this->redirect = $redirect;
         $this->httpResponse = $httpResponse;
     }
 
     public function execute(Observer $observer): void
     {
-        if ($this->isCaptchaEnabled->isCaptchaEnabledFor(self::RECAPTCHA_FORM_KEY)) {
-            $request = $observer->getRequest();
-            $this->requestHandler->execute(
-                self::RECAPTCHA_FORM_KEY,
-                $request,
-                $this->httpResponse,
-                $this->redirect->getRefererUrl()
-            );
+        if (!($this->isCaptchaEnabled !== null
+            && $this->requestHandler !== null
+            && $this->isCaptchaEnabled->isCaptchaEnabledFor(self::RECAPTCHA_FORM_KEY))) {
+            return;
         }
+
+        $request = $observer->getRequest();
+        $this->requestHandler->execute(
+            self::RECAPTCHA_FORM_KEY,
+            $request,
+            $this->httpResponse,
+            $this->redirect->getRefererUrl()
+        );
     }
 }
